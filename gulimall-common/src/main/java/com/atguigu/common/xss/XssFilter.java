def2307eb22447cef2328305394c000aa258bf92
engineerping <gongchengship@163.com>
2023-07-18 16:53:11 +0800

    可运行:gulimall-product 模块集成 Mybatis-plus 之后，但愿成功，CRUD成功

--- gulimall-common/src/main/java/com/atguigu/common/xss/XssFilter.java
+++ gulimall-common/src/main/java/com/atguigu/common/xss/XssFilter.java
@@ -1,40 +1,39 @@
-/**
- * Copyright (c) 2016-2019 人人开源 All rights reserved.
- *
- * https://www.renren.io
- *
- * 版权所有，侵权必究！
- */
-
-package com.atguigu.common.xss;
-
-import javax.servlet.*;
-import javax.servlet.http.HttpServletRequest;
-import java.io.IOException;
-
-/**
- * XSS过滤
- *
- * @author Mark sunlightcs@gmail.com
- *
- * This class is copied from
- * renren-fast/src/main/java/io/renren/common/xss
- */
-public class XssFilter implements Filter {
-
-	@Override
-	public void init(FilterConfig config) throws ServletException {
-	}
-
-	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-            throws IOException, ServletException {
-		XssHttpServletRequestWrapper xssRequest = new XssHttpServletRequestWrapper(
-				(HttpServletRequest) request);
-		chain.doFilter(xssRequest, response);
-	}
-
-	@Override
-	public void destroy() {
-	}
-
-}
+//*
+// * Copyright (c) 2016-2019 人人开源 All rights reserved.
+// *
+// * https://www.renren.io
+// *
+// * 版权所有，侵权必究！
+//
+//package com.atguigu.common.xss;
+//
+//import javax.servlet.*;
+//import javax.servlet.http.HttpServletRequest;
+//import java.io.IOException;
+//
+///**
+// * XSS过滤，（这个类使用来放置 XSS 攻击的在后面的课中删掉了，因为使用 spring security 替换掉了）
+// *
+// * @author Mark sunlightcs@gmail.com
+// *
+// * This class is copied from
+// * renren-fast/src/main/java/io/renren/common/xss
+// */
+//public class XssFilter implements Filter {
+//
+//	@Override
+//	public void init(FilterConfig config) throws ServletException {
+//	}
+//
+//	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+//            throws IOException, ServletException {
+//		XssHttpServletRequestWrapper xssRequest = new XssHttpServletRequestWrapper(
+//				(HttpServletRequest) request);
+//		chain.doFilter(xssRequest, response);
+//	}
+//
+//	@Override
+//	public void destroy() {
+//	}
+//
+//}
