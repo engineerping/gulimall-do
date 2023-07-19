231f4a5e39a684e3f7205fbd214196721277ef41
engineerping <gongchengship@163.com>
2023-07-19 12:17:38 +0800

    可运行: 更新单元测试

--- gulimall-product/src/test/java/com/atguigu/gulimall/product/service/impl/BrandServiceTest.java
+++ gulimall-product/src/test/java/com/atguigu/gulimall/product/service/impl/BrandServiceTest.java
@@ -5,7 +5,9 @@ import com.atguigu.gulimall.product.dao.BrandDao;
 import com.atguigu.gulimall.product.entity.BrandEntity;
 import com.atguigu.gulimall.product.service.BrandService;
 import com.baomidou.mybatisplus.core.conditions.Wrapper;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.conditions.segments.MergeSegments;
+import com.baomidou.mybatisplus.extension.service.additional.query.impl.QueryChainWrapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +15,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.HashMap;
+import java.util.List;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
@@ -32,10 +35,12 @@ public class BrandServiceTest {
         brandDao.deleteAll();
         brandService.save(brandEntity);
 
-        HashMap<String, String> queryParam = new HashMap<>();
+        // List<BrandEntity> list = brandService.list(new QueryWrapper<BrandEntity>().eq("name", "华为"));
+        // list.forEach(System.out::println);
+        HashMap<String, Object> queryParam = new HashMap<>();
         queryParam.put("page", "1");
         queryParam.put("limit", "10");
-        PageUtils pageUtils = (PageUtils)brandService.queryPage(new HashMap<>());
+        PageUtils pageUtils = (PageUtils)brandService.queryPage(queryParam);
         BrandEntity brand = (BrandEntity)pageUtils.getList().get(0);
         brand.getName().equals("华为");
         //delete the inserted value
