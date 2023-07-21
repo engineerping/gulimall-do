c7dd7cf3d8b43998a6003922946435135486c374
engineerping <gongchengship@163.com>
2023-07-21 11:11:34 +0800

    可运行: 使用 Nacos 作为注册中心，并将gulimall 所有的服务注册在Nacos 中成功

--- gulimall-member/src/main/java/com/atguigu/gulimall/member/GulimallMemberApplication.java
+++ gulimall-member/src/main/java/com/atguigu/gulimall/member/GulimallMemberApplication.java
@@ -1,9 +1,13 @@
 package com.atguigu.gulimall.member;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@MapperScan("com.atguigu.gulimall.member.dao")
 @SpringBootApplication
+@EnableDiscoveryClient
 public class GulimallMemberApplication {
 
 	public static void main(String[] args) {
--- gulimall-order/src/main/java/com/atguigu/gulimall/order/GulimallOrderApplication.java
+++ gulimall-order/src/main/java/com/atguigu/gulimall/order/GulimallOrderApplication.java
@@ -1,9 +1,13 @@
 package com.atguigu.gulimall.order;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@MapperScan("com.atguigu.gulimall.order.dao")
 @SpringBootApplication
+@EnableDiscoveryClient
 public class GulimallOrderApplication {
 
 	public static void main(String[] args) {
