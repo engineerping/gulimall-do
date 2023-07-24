5bf25c951f5d21dc33d85f8f31b05602dfbe40c5
engineerping <gongchengship@163.com>
2023-07-24 12:27:54 +0800

    fix springboot startup 报错:Either 'name' or 'value' must be provided in @FeignClient。->将暂时用不到的 @FeignClient 都注释掉了， 改成了 //@FeignClient //Either 'name' or 'value' must be provided in @FeignClient

--- gulimall-coupon/src/main/java/com/atguigu/gulimall/coupon/feign/FetchService.java
+++ gulimall-coupon/src/main/java/com/atguigu/gulimall/coupon/feign/FetchService.java
@@ -2,7 +2,7 @@ package com.atguigu.gulimall.coupon.feign;
 
 import org.springframework.cloud.openfeign.FeignClient;
 
-@FeignClient
+//@FeignClient //Either 'name' or 'value' must be provided in @FeignClient
 public interface FetchService {
 
 }
--- gulimall-product/src/main/java/com/atguigu/gulimall/product/feign/FetchService.java
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/feign/FetchService.java
@@ -2,7 +2,7 @@ package com.atguigu.gulimall.product.feign;
 
 import org.springframework.cloud.openfeign.FeignClient;
 
-@FeignClient
+//@FeignClient //Either 'name' or 'value' must be provided in @FeignClient
 public interface FetchService {
 
 }
