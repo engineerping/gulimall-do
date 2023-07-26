b393be0520ce3bf53263735e4adc2de976554bdc
engineerping <gongchengship@163.com>
2023-07-26 14:12:25 +0800

    可运行，使用 spring cloud gateway 做 spring cloud 的网关成功, 成功

---
+++ gulimall-gateway/src/main/java/com/atguigu/gulimall/gateway/GulimallGatewayApplication.java
@@ -0,0 +1,16 @@
+package com.atguigu.gulimall.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient //让自己被注册中心发现
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class}) //排除数据池，避免报
+public class GulimallGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GulimallGatewayApplication.class, args);
+	}
+
+}
